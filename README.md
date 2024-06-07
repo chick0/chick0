cf3b4ed4f94deca1577f8de44fb01a3d0b11ff71
chick_0 <chick_0@kakao.com>
2024-06-08 01:33:44 +0900

    svelte로 전환

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <p align="center">
     <img width="830px" src="https://user-images.githubusercontent.com/64462443/135767852-b1d12c25-d60c-4ff2-ad50-70c8b7c46d47.png">
 </p>
+
+[Link](https://ch1ck.xyz/)
---
+++ src/lib/main/Section.svelte
@@ -0,0 +1,116 @@
+<script>
+    /** @type {String} 섹션 ID */
+    export let id
+
+    /** @type {String} 제목 */
+    export let title
+
+    /** @type {String} 부제목 */
+    export let subtitle = ""
+
+    /** @type {String} 배경 이미지 혹은 색상 */
+    export let background = "#1f1f1f"
+
+    /** @type {("center" | "left"| "right")} 배경 이미지 위치 */
+    export let position = "center"
+
+    /** @type {String[]} 텍스트 내용들 */
+    export let content = []
+
+    /**
+     * @typedef Link 링크 정보
+     * @property {string} label 표시 텍스트
+     * @property {string} src 링크 주소
+     * @property {string} color 버튼 배경 색상
+     * @property {string} [text] 버튼 텍스트 색상
+     * @property {string} [icon] 아이콘
+     */
+
+    /** @type {Link[]} 링크 목록 */
+    export let links = []
+</script>
+
+<div
+    {id}
+    class="section"
+    style="--bg: {background.startsWith('#') ? background : `url(${background})`}; --position: {position}">
+    <div>
+        <h1 class="title">{title}</h1>
+        <p class="subtitle">{@html subtitle}</p>
+
+        {#each content as line}
+            <p>{@html line}</p>
+        {/each}
+
+        {#if links.length != 0}
+            <br />
+            <div class="group">
+                {#each links as link}
+                    <a href={link.src} style="--background: {link.color}; --text: {link.text ?? '#fff'}">
+                        {#if link.icon != null}
+                            <i class="fa {link.icon}"></i>
+                        {/if}
+                        {link.label}
+                    </a>
+                {/each}
+            </div>
+        {/if}
+    </div>
+</div>
+
+<style scoped>
+    .section {
+        width: 100%;
+        height: 100vh;
+        color: #fff;
+
+        background: var(--bg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: var(--position);
+    }
+
+    .section > div {
+        position: relative;
+        top: 50%;
+        transform: translateY(-50%);
+
+        width: 100%;
+
+        padding-left: 250px;
+        padding-right: 250px;
+    }
+
+    @media (max-width: 768px) {
+        .section > div {
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+    }
+
+    @media (min-width: 768px) and (max-width: 1200px) {
+        .section > div {
+            padding-left: 50px;
+            padding-right: 50px;
+        }
+    }
+
+    .title {
+        font-size: 45px;
+        font-weight: bold;
+
+        margin-bottom: 20px;
+    }
+
+    .subtitle {
+        font-size: 22px;
+
+        margin-top: -20px;
+        margin-bottom: 20px;
+    }
+
+    /* icon margin */
+    .fa {
+        margin-right: 3px;
+    }
+</style>
