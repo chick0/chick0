e5ce4f4766ad9d3867e5673c1829ed53e0e20f06
chick_0 <chick_0@kakao.com>
2024-06-08 02:16:47 +0900

    오류 페이지 추가

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { page } from "$app/stores"
+    import Section from "$lib/main/Section.svelte"
+
+    import bg_main from "$lib/assets/main/bg_main.jpg"
+</script>
+
+<Section
+    id="error"
+    title={$page.status?.toString() ?? "오류"}
+    subtitle={$page.error?.message ?? "알 수 없는 오류가 발생했습니다."}
+    background={bg_main}
+    links={[
+        {
+            label: "메인 화면으로 이동",
+            src: "/",
+            color: "#000",
+            icon: "fa-home",
+        },
+    ]} />
