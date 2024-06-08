ed408739fb9607f8aea88cbd5b345c9036f9b90f
chick_0 <chick_0@kakao.com>
2024-06-09 00:18:25 +0900

    미사용 언어 설정 삭제

--- src/lib/hljs.js
+++ src/lib/hljs.js
@@ -1,12 +1,9 @@
 import hljs from "highlight.js/lib/core"
-import javascript from "highlight.js/lib/languages/javascript"
 import csharp from "highlight.js/lib/languages/csharp"
 
-hljs.registerLanguage("javascript", javascript)
-hljs.registerLanguage("cs", csharp)
-
-console.log(hljs.listLanguages().length)
 import "highlight.js/styles/atom-one-dark.min.css"
+
+hljs.registerLanguage("cs", csharp)
 
 export function highlightAll() {
     hljs.highlightAll()
