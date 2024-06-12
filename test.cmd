6171f89b47bde138af61cfca2808352295183f71
chick_0 <chick_0@kakao.com>
2024-06-13 07:28:39 +0900

    로컬 테스트 스크립트 수정

--- test.cmd
+++ test.cmd
@@ -1,2 +1,12 @@
 @ECHO OFF
+
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+
+ECHO * Server: http://localhost
 python -m http.server -d build 80
+
+goto:eof
+
+:errorNoPython
+echo Error^: Python not installed
