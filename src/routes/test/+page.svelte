fb427c39437aa183efe7cdcf090ab98ee514af23
chick_0 <chick_0@kakao.com>
2024-06-13 19:08:13 +0900

    테스트용 페이지 추가

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Content from "$lib/component/Content.svelte"
+    import ContentGroup from "$lib/component/ContentGroup.svelte"
+
+    let result = "READY"
+    let command = ""
+</script>
+
+<Content>
+    <ContentGroup>
+        <input bind:value={command} />
+        <button
+            on:click={() => {
+                try {
+                    result = JSON.stringify(eval(command))
+                } catch (e) {
+                    // @ts-ignore
+                    result = e.toString()
+                }
+            }}>Run</button>
+    </ContentGroup>
+
+    <hr />
+
+    <pre>{result}</pre>
+</Content>
+
+<style>
+    input {
+        width: calc(100% - 100px);
+    }
+
+    button {
+        width: 80px;
+    }
+</style>
